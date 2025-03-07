fb4daa729e7ed44c24328c44af2fea9bf27e19d6
CrabeNeo <neo.becogne@iut-rodez.fr>
2025-03-07 10:31:59 +0100

    ajout fonction getIncident

--- API/donnees.php
+++ API/donnees.php
@@ -35,8 +35,32 @@
     function getIncident() {
         try {
             $pdo=connecteBD();
-            $maReqete='SELECT'
-        }
+            $maReqete='SELECT id_incident,resume,description,service_technique
+                                ,incident.id_reservation,intitule,reservation.date_reservation,
+                                reservation.heure_debut,reservation.heure_fin, salle.nom,
+                                incident.date_signalement,incident.heure_signalement
+                                FROM incident
+                                JOIN gravite
+                                ON incident.id_gravite = gravite.id_gravite
+                                JOIN reservation
+                                ON incident.id_reservation = reservation.id_reservation
+                                JOIN salle
+                                ON reservation.id_salle = salle.id_salle';
+        $stmt = $pdo->prepare($maRequete);										// Préparation de la requête
+        $stmt->execute();	
+            
+        $incidents=$stmt ->fetchALL();
+        $stmt->closeCursor();
+        $stmt=null;
+        $pdo=null;
+
+        sendJSON($incidents, 200) ;
+        
+        }catch(PDOException $e){
+			$infos['Statut']="KO";
+			$infos['message']=$e->getMessage();
+			sendJSON($infos, 500) ;
+		}
     }
     function putIncident() {
         try {
