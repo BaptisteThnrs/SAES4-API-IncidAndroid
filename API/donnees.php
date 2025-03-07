e7f716f218c96527b176e708ddc9d49fcda3f3c8
BaptisteThnrs <baptiste@thenieres.fr>
2025-03-07 09:55:10 +0100

    init

---
+++ API/donnees.php
@@ -0,0 +1,18 @@
+<?php
+    include 'liaisonBD.php';
+    $pdo = connecteBD();
+
+    function getReservation() {
+        try {
+            $pdo=connecteBD();
+            $maReqete='SELECT'
+        }
+    }
+
+    function getIncident() {
+        try {
+            $pdo=connecteBD();
+            $maReqete='SELECT'
+        }
+    }
+?>
