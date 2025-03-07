e5e6bb370bbbff88d57532a9a63cd866b0e92692
BaptisteThnrs <baptiste@thenieres.fr>
2025-03-07 10:28:59 +0100

    ajout méthode recuperation des réservations

--- API/donnees.php
+++ API/donnees.php
@@ -5,7 +5,30 @@
     function getReservation() {
         try {
             $pdo=connecteBD();
-            $maReqete='SELECT'
+            $maReqete='SELECT reservation.id_reservation as id_reservation, salle.nom as nom_salle, employe.nom as nom_employe,
+                        employe.prenom as prenom_employe, activite.nom_activite as nom_activite, reservation.date_reservation 
+                        as date, reservation.heure_debut as heure_debut, reservation.heure_fin as heure_fin, reservation.id_employe as id_employe
+                        FROM reservation
+                        JOIN salle
+                        ON reservation.id_salle = salle.id_salle
+                        JOIN employe
+                        ON reservation.id_employe = employe.id_employe
+                        JOIN activite
+                        ON reservation.id_activite = activite.id_activite
+                        ORDER BY date DESC';
+            $stmt = $pdo->prepare($maReqete);
+            $stmt->execute();
+
+            $reservations=$stmt->fetchALL();
+			$stmt->closeCursor();
+			$stmt=null;
+			$pdo=null;
+
+			sendJSON($reservations, 200);
+		} catch(PDOException $e){
+			$infos['Statut']="KO";
+			$infos['message']=$e->getMessage();
+			sendJSON($infos, 500) ;
         }
     }
 
