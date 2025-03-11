3e092771b1b70fc4769c66db5c0aadb5d5119020
BaptisteThnrs <baptiste@thenieres.fr>
2025-03-11 10:04:31 +0100

    fichiers pour docker et phpstan

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e01f72d00d1a969d6f8ba0516a95ad6939b200b2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e01f72d00d1a969d6f8ba0516a95ad6939b200b2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.12.21',
+            'version' => '1.12.21.0',
+            'reference' => '14276fdef70575106a3392a4ed553c06a984df28',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
