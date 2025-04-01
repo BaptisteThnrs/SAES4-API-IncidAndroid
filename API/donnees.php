b480919d2b77e3e2a7993734f35751b2eb68fc23
Alexandreiut <131276833+Alexandreiut@users.noreply.github.com>
2025-04-01 10:32:58 +0200

    Update donnees.php

--- API/donnees.php
+++ API/donnees.php
@@ -41,8 +41,7 @@ class Donnees {
             $maRequete = 'SELECT id_incident, resume, description, service_technique,
                                  incident.id_reservation, intitule, reservation.date_reservation,
                                  reservation.heure_debut, reservation.heure_fin, salle.nom,
-                                 incident.date_signalement, incident.heure_signalement, incident.id_employe,
-                                 gravite.id_gravite
+                                 incident.date_signalement, incident.heure_signalement, incident.id_employe
                           FROM incident
                           JOIN gravite ON incident.id_gravite = gravite.id_gravite
                           JOIN reservation ON incident.id_reservation = reservation.id_reservation
